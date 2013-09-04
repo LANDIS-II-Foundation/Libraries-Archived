e12b5bd5fbbeb7de6be6fcf3e6c912a3919f66f2
Rob <rmscheller@gmail.com>
2013-09-04 00:21:35 +0000

    git-svn-id: http://Marc-PC/svn/Full@1256 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/clmate-generator-library/branches/amin-climate/amin-climate/NoClimateChangeMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Landis.Library.Climate
+{
+    public enum NoClimateChangeMode
+    {
+        spinup_Average = 0,
+        spinup_Random = 1,
+    }
+}
