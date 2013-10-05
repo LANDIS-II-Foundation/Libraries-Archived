863bc6c43322375c30b277acbc624ee37ed333d3
Rob <rmscheller@gmail.com>
2013-10-05 20:35:50 +0000

    Testing options for generating spp data.
    git-svn-id: http://Marc-PC/svn/Full@1309 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/metadata/branches/rob-metadata/MapDataType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Landis.Library.Metadata
+{
+    public enum MapDataType
+    {
+        Nominal = 0,
+        Ordinal = 1,
+        Quantitative = 2,
+        Scale = 3,
+    }
+}
---
+++ trunk/metadata/branches/rob-metadata/OutputType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace Landis.Library.Metadata
+{
+    public enum OutputType
+    {
+        Map = 0,
+        Table = 1,
+    }
+}
