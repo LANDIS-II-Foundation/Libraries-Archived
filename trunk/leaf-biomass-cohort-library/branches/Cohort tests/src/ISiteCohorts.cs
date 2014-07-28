007432eb576bf413b1e2d21327e673094d02fa38
Brian <brmirand@gmail.com>
2014-07-28 19:59:17 +0000

    Add branch for testing cohort inheritance
    git-svn-id: http://Marc-PC/svn/Full@2049 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/leaf-biomass-cohort-library/branches/Cohort tests/src/ISiteCohorts.cs
@@ -0,0 +1,20 @@
+using Landis.Core;
+//using Landis.Cohorts;
+using Landis.Library.AgeOnlyCohorts;
+using Landis.SpatialModeling;
+
+namespace Landis.Library.LeafBiomassCohorts
+{
+    /// <summary>
+    /// All the biomass cohorts at a site.
+    /// </summary>
+    public interface ISiteCohorts
+        : Landis.Library.Cohorts.ISiteCohorts<ISpeciesCohorts>
+    {
+        
+        int ReduceOrKillBiomassCohorts(IDisturbance disturbance);
+        void AddNewCohort(ISpecies species, ushort age, float initialWood, float initialLeaf);
+        void Grow(ActiveSite site, bool isSuccessionTimestep, bool annualTimestep);
+        
+    }
+}
