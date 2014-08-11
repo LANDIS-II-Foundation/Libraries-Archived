db78743418c277c02cbac4de1766a83b60c61c7a
jimm <jdomingo@green-code.com>
2014-08-11 03:25:26 +0000

    Removed commented out code.
    git-svn-id: http://Marc-PC/svn/Full@2179 85f15e0e-5137-b144-a863-98bd542365a3

--- trunk/succession-library/trunk/src/Properties/AssemblyInfo.cs
+++ trunk/succession-library/trunk/src/Properties/AssemblyInfo.cs
@@ -9,15 +9,4 @@
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-//[assembly: AssemblyVersion("0.0.0.0")]
-//[assembly: AssemblyFileVersion("0.0.0.0")]
-
 [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
