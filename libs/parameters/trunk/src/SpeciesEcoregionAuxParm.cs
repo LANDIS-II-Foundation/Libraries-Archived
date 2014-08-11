f026d5a565cce446fd73728a72a5dc8799306656
jimm <jdomingo@green-code.com>
2014-08-11 12:32:23 +0000

    Copied the species-ecoregion auxiliary parameter class from the Biomass library.
    git-svn-id: http://Marc-PC/svn/Full@2196 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ libs/parameters/trunk/src/SpeciesEcoregionAuxParm.cs
@@ -0,0 +1,45 @@
+﻿using Landis.Core;
+
+namespace Landis.Library.Biomass
+{
+   public class SpeciesEcoregionAuxParm<T>
+    {
+       Landis.Library.Biomass.Species.AuxParm<Landis.Library.Biomass.Ecoregions.AuxParm<T>> values;
+
+        /*public Landis.Library.Biomass.Species.AuxParm<Landis.Library.Biomass.Ecoregions.AuxParm<T>> Parm
+        {
+            get
+            {
+                return values;
+            }
+            set
+            {
+                values = value;
+            }
+        }
+         * */
+
+        public T this[ISpecies species, IEcoregion ecoregion]
+        {
+            get
+            {
+                return values[species][ecoregion];
+            }
+
+            set
+            {
+                values[species][ecoregion] = value;
+            }
+        }
+
+        public SpeciesEcoregionAuxParm(ISpeciesDataset speciesDataset, IEcoregionDataset ecoregionDataset)
+        {
+            values = new Landis.Library.Biomass.Species.AuxParm<Landis.Library.Biomass.Ecoregions.AuxParm<T>>(speciesDataset);
+            foreach (ISpecies species in speciesDataset)
+            {
+                values[species] = new Landis.Library.Biomass.Ecoregions.AuxParm<T>(ecoregionDataset);
+            }
+        }
+        //---------------------------------------------------------------------
+    }
+}
