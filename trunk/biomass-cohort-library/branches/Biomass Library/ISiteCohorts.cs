9e83fc5a5a635dcde96e95948974a0a68968db09
Brian <brmirand@gmail.com>
2014-08-14 18:36:33 +0000

    Removed succession-specific functions (grow, addnewcohort) from ISiteCohorts. Set version number to 1.3.
    git-svn-id: http://Marc-PC/svn/Full@2228 85f15e0e-5137-b144-a863-98bd542365a3

--- trunk/biomass-cohort-library/branches/Biomass Library/ISiteCohorts.cs
+++ trunk/biomass-cohort-library/branches/Biomass Library/ISiteCohorts.cs
@@ -1,10 +1,6 @@
 //  Copyright 2005-2010 Portland State University, University of Wisconsin
 //  Authors:  Robert M. Scheller, James B. Domingo
 
-using Landis.Core;
-using Landis.Library.AgeOnlyCohorts;
-using Landis.SpatialModeling;
-
 namespace Landis.Library.BiomassCohorts
 {
     /// <summary>
@@ -22,9 +18,5 @@ namespace Landis.Library.BiomassCohorts
         /// The total of all the cohorts' biomass reductions.
         /// </returns>
         int ReduceOrKillBiomassCohorts(IDisturbance disturbance);
-        
-        void AddNewCohort(ISpecies species, ushort age, int initialBiomass);
-        string Write();
-        void Grow(ActiveSite site, bool isSuccessionTimestep);
     }
 }
