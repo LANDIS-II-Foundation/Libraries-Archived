2191d56ad429c091372fa8a786e26043f40c8819
jimm <jdomingo@green-code.com>
2014-10-21 01:56:07 +0000

    Moved the interface for cohort harvest and its whole cohort implementation to the Site Harvest library so they are available for harvest extensions.
    git-svn-id: http://Marc-PC/svn/Full@2520 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ libs/site-harvest/trunk/src/ICohortHarvest.cs
@@ -0,0 +1,20 @@
+﻿// This file is part of the Land Use extension for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/exts/land-use/trunk/
+
+using Landis.SpatialModeling;
+
+namespace Landis.Extension.LandUse.LandCover
+{
+    /// <summary>
+    /// Interface for cutting cohorts at a site.
+    /// </summary>
+    interface ICohortHarvest
+    {
+        /// <summary>
+        /// Cut cohorts at an individual site.
+        /// </summary>
+        void Cut(ActiveSite site);
+    }
+}
---
+++ libs/site-harvest/trunk/src/WholeCohortHarvest.cs
@@ -0,0 +1,41 @@
+﻿// This file is part of the Land Use extension for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/exts/land-use/trunk/
+
+using Landis.Library.Harvest;
+using Landis.SpatialModeling;
+using log4net;
+
+namespace Landis.Extension.LandUse.LandCover
+{
+    /// <summary>
+    /// A harvest where each selected cohort is completely cut.
+    /// </summary>
+    class WholeCohortHarvest
+        : AgeCohortHarvest, ICohortHarvest
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(WholeCohortHarvest));
+        private static readonly bool isDebugEnabled = log.IsDebugEnabled;
+
+        //---------------------------------------------------------------------
+
+        public WholeCohortHarvest(ICohortSelector cohortSelector)
+            : base(cohortSelector)
+        {
+            base.Type = Main.ExtType;
+        }
+
+        //---------------------------------------------------------------------
+
+        void ICohortHarvest.Cut(ActiveSite site)
+        {
+            if (isDebugEnabled)
+                log.DebugFormat("    {0} is cutting site {1}:",
+                                GetType().Name,
+                                site.Location);
+            CurrentSite = site;
+            base.Cut(site);
+        }
+    }
+}
