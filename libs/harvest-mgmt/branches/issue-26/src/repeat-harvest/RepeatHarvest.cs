d7591b9c4f626ba93b4eccaf5ee9b24f64538c7f
lbross@pdx.edu <lbross@pdx.edu>
2014-10-22 17:36:17 +0000

    Merge changes from harvest-mgmt trunk
    git-svn-id: http://Marc-PC/svn/Full@2554 85f15e0e-5137-b144-a863-98bd542365a3

--- libs/harvest-mgmt/branches/issue-26/src/repeat-harvest/RepeatHarvest.cs
+++ libs/harvest-mgmt/branches/issue-26/src/repeat-harvest/RepeatHarvest.cs
@@ -1,8 +1,9 @@
-// This file is part of the Harvest library for LANDIS-II.
+// This file is part of the Harvest Management library for LANDIS-II.
 // For copyright and licensing information, see the NOTICE and LICENSE
 // files in this project's top-level directory, and at:
-//   http://landis-extensions.googlecode.com/svn/libs/harvest/trunk/
+//   http://landis-extensions.googlecode.com/svn/libs/harvest-mgmt/trunk/
 
+using Landis.Library.SiteHarvest;
 using Landis.Library.Succession;
 using System.Collections.Generic;
 
