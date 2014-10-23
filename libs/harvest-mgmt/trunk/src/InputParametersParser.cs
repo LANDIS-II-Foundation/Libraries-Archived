dfa0dd60e3b7b79bf798c6b11862020e535a0dbe
jimm <jdomingo@green-code.com>
2014-10-23 17:00:03 +0000

    Updated the InputParametersParser class so it now derives from the BasicParameterParser class in the Site Harvest library.  This new base class enabled the removal of several duplicated methods in the derived class.
    git-svn-id: http://Marc-PC/svn/Full@2564 85f15e0e-5137-b144-a863-98bd542365a3

--- libs/harvest-mgmt/trunk/src/InputParametersParser.cs
+++ libs/harvest-mgmt/trunk/src/InputParametersParser.cs
@@ -18,7 +18,7 @@ namespace Landis.Library.Harvest
     /// A parser that reads harvest parameters from text input.
     /// </summary>
     public class InputParametersParser
-        : TextParser<IInputParameters>
+        : BasicParameterParser<IInputParameters>
     {
         private static ParseMethod<ushort> uShortParse;
         private string extensionName;
@@ -36,10 +36,10 @@ namespace Landis.Library.Harvest
             public const string MinimumAge = "MinimumAge";
             public const string spatialArrangement = "SpatialArrangement";
             public const string minimumTimeSinceLastHarvest = "MinimumTimeSinceLastHarvest";
-            public const string PreventEstablishment = "PreventEstablishment";
+            public const string PreventEstablishment = ParameterNames.PreventEstablishment;
             public const string MultipleRepeat = "MultipleRepeat";
             public const string MinTimeSinceDamage = "MinTimeSinceDamage";
-            public const string Plant = "Plant";
+            public const string Plant = ParameterNames.Plant;
             public const string Prescription = "Prescription";
             public const string PrescriptionMaps = "PrescriptionMaps";
             public const string SingleRepeat = "SingleRepeat";
@@ -145,6 +145,8 @@ namespace Landis.Library.Harvest
         /// </param>
         public InputParametersParser(string          extensionName,
                                      ISpeciesDataset speciesDataset)
+            : base(speciesDataset, true)
+            // The "true" above --> keywords are enabled for cohort selectors
         {
             this.extensionName = extensionName;
             this.speciesDataset = speciesDataset;
@@ -241,13 +243,7 @@ namespace Landis.Library.Harvest
                 }
 
                 //get preventEstablishment
-                bool preventEstablishment  = false;
-                if (ReadOptionalName(Names.PreventEstablishment))
-                    preventEstablishment = true;
-
-                //InputVar<bool> preventEstVar = new InputVar<bool>("PreventEstablishment");
-                //if (ReadOptionalVar(preventEstVar))
-                //    preventEstablishment = preventEstVar.Value;
+                bool preventEstablishment  = ReadPreventEstablishment();
 
                 //get cohort selection method
                 ICohortSelector cohortSelector = ReadCohortSelector(false);
@@ -529,35 +525,6 @@ namespace Landis.Library.Harvest
 
         //---------------------------------------------------------------------
 
-        protected ISpecies ReadSpecies(StringReader currentLine)
-        {
-            ISpecies species = ReadAndValidateSpeciesName(currentLine);
-            int lineNumber;
-            if (speciesLineNumbers.TryGetValue(species.Name, out lineNumber))
-                throw new InputValueException(speciesName.Value.String,
-                                              "The species {0} was previously used on line {1}",
-                                              speciesName.Value.String, lineNumber);
-            else
-                speciesLineNumbers[species.Name] = LineNumber;
-
-            return species;
-        }
-
-        //---------------------------------------------------------------------
-
-        protected ISpecies ReadAndValidateSpeciesName(StringReader currentLine)
-        {
-            ReadValue(speciesName, currentLine);
-            ISpecies species = speciesDataset[speciesName.Value.Actual];
-            if (species == null)
-                throw new InputValueException(speciesName.Value.String,
-                                              "{0} is not a species name",
-                                              speciesName.Value.String);
-            return species;
-        }
-
-        //---------------------------------------------------------------------
-
 
         private static List<string> namesThatFollowForestType = new List<string>(
             new string[]{
@@ -785,197 +752,6 @@ namespace Landis.Library.Harvest
             throw new InputValueException(cohortSelection.Value.String,
                                           cohortSelection.Value.String + " is not a valid cohort selection",
                                           new MultiLineText("Valid values: ClearCut or SpeciesList"));
-        }
-
-        //---------------------------------------------------------------------
-
-        /// <summary>
-        /// Reads a list of species and their cohorts that should be removed.
-        /// </summary>
-        protected ICohortSelector ReadSpeciesAndCohorts(params string[] names)
-        {
-            List<string> namesThatFollow;
-            if (names == null)
-                namesThatFollow = new List<string>();
-            else
-                namesThatFollow = new List<string>(names);
-
-            MultiSpeciesCohortSelector cohortSelector = new MultiSpeciesCohortSelector();
-            speciesLineNumbers.Clear();
-
-            while (! AtEndOfInput && ! namesThatFollow.Contains(CurrentName)) {
-                StringReader currentLine = new StringReader(CurrentLine);
-
-                // Species name
-                ISpecies species = ReadSpecies(currentLine);
-
-                //  Cohort keyword, cohort age or cohort age range
-                //  keyword = (All, Youngest, AllExceptYoungest, Oldest,
-                //             AllExceptOldest, 1/{N})
-                TextReader.SkipWhitespace(currentLine);
-                int indexOfDataAfterSpecies = currentLine.Index;
-                string word = TextReader.ReadWord(currentLine);
-                if (word == "")
-                    throw NewParseException("No cohort keyword, age or age range after the species name");
-
-                bool isKeyword = false;
-                if (word == "All") {
-                    cohortSelector[species] = SelectCohorts.All;
-                    isKeyword = true;
-                }
-                else if (word == "Youngest") {
-                    cohortSelector[species] = SelectCohorts.Youngest;
-                    isKeyword = true;
-                }
-                else if (word == "AllExceptYoungest") {
-                    cohortSelector[species] = SelectCohorts.AllExceptYoungest;
-                    isKeyword = true;
-                }
-                else if (word == "Oldest") {
-                    cohortSelector[species] = SelectCohorts.Oldest;
-                    isKeyword = true;
-                }
-                else if (word == "AllExceptOldest") {
-                    cohortSelector[species] = SelectCohorts.AllExceptOldest;
-                    isKeyword = true;
-                }
-                else if (word.StartsWith("1/")) {
-                    InputVar<ushort> N = new InputVar<ushort>("1/N");
-                    N.ReadValue(new StringReader(word.Substring(2)));
-                    if (N.Value.Actual == 0)
-                        throw NewParseException("For \"1/N\", N must be > 0");
-                    cohortSelector[species] = new EveryNthCohort(N.Value.Actual).SelectCohorts;
-                    isKeyword = true;
-                }
-
-                if (isKeyword)
-                    CheckNoDataAfter("the keyword \"" + word + "\"", currentLine);
-                else {
-                    //  Read one or more ages or age ranges
-                    List<ushort> ages = new List<ushort>();
-                    List<AgeRange> ranges = new List<AgeRange>();
-                    currentLine = new StringReader(CurrentLine.Substring(indexOfDataAfterSpecies));
-                    InputVar<AgeRange> ageOrRange = new InputVar<AgeRange>("Age or Age Range");
-                    while (currentLine.Peek() != -1) {
-                        ReadValue(ageOrRange, currentLine);
-                        ValidateAgeOrRange(ageOrRange.Value, ages, ranges);
-                        TextReader.SkipWhitespace(currentLine);
-                    }
-                    cohortSelector[species] = new SpecificAgesCohortSelector(ages, ranges).SelectCohorts;
-                }
-
-                GetNextLine();
-            }
-
-            if (speciesLineNumbers.Count == 0)
-                throw NewParseException("Expected a line starting with a species name");
-
-            return cohortSelector;
-        }
-
-        //---------------------------------------------------------------------
-
-        /// <summary>
-        /// Validates a cohort age or age range against previous ages and
-        /// ranges.
-        /// </summary>
-        /// <param name="ageOrRange">
-        /// The age or age range that's being validated.
-        /// </param>
-        /// <param name="ages">
-        /// List of previous ages.
-        /// </param>
-        /// <param name="ranges">
-        /// List of previous ranges.
-        /// </param>
-        /// <remarks>
-        /// If the age or range is validated, it is added to the corresponding
-        /// list.
-        /// </remarks>
-        protected void ValidateAgeOrRange(InputValue<AgeRange> ageOrRange,
-                                          List<ushort>         ages,
-                                          List<AgeRange>       ranges)
-        {
-            if (ageOrRange.String.Contains("-")) {
-                AgeRange range = ageOrRange.Actual;
-
-                //  Does the range contain any individual ages?
-                foreach (ushort age in ages) {
-                    if (range.Contains(age))
-                        throw new InputValueException(ageOrRange.String,
-                                                      "The range {0} contains the age {1}",
-                                                      ageOrRange.String, age);
-                }
-
-                //  Does the range overlap any previous ranges?
-                foreach (AgeRange previousRange in ranges) {
-                    if (range.Overlaps(previousRange))
-                        throw new InputValueException(ageOrRange.String,
-                                                      "The range {0} overlaps the range {1}-{2}",
-                                                      ageOrRange.String, previousRange.Start, previousRange.End);
-                }
-
-                ranges.Add(range);
-            }
-            else {
-                ushort age = ageOrRange.Actual.Start;
-
-                //  Does the age match any of the previous ages?
-                foreach (ushort previousAge in ages) {
-                    if (age == previousAge)
-                        throw new InputValueException(ageOrRange.String,
-                                                      "The age {0} appears more than once",
-                                                      ageOrRange.String);
-                }
-
-                //  Is the age in any of the previous ranges?
-                foreach (AgeRange previousRange in ranges) {
-                    if (previousRange.Contains(age))
-                        throw new InputValueException(ageOrRange.String,
-                                                      "The age {0} lies within the range {1}-{2}",
-                                                      ageOrRange.String, previousRange.Start, previousRange.End);
-                }
-
-                ages.Add(age);
-            }
-        }
-
-        //---------------------------------------------------------------------
-
-        protected Planting.SpeciesList ReadSpeciesToPlant()
-        {
-            InputVar<List<ISpecies>> plant = new InputVar<List<ISpecies>>("Plant", ReadSpeciesList);
-            if (ReadOptionalVar(plant))
-                return new Planting.SpeciesList(plant.Value.Actual, speciesDataset); //Model.Core.Species);
-            else
-                return null;
-        }
-
-        //---------------------------------------------------------------------
-
-        public InputValue<List<ISpecies>> ReadSpeciesList(StringReader currentLine,
-                                                          out int      index)
-        {
-            List<string> speciesNames = new List<string>();
-            List<ISpecies> speciesList = new List<ISpecies>();
-
-            TextReader.SkipWhitespace(currentLine);
-            index = currentLine.Index;
-            while (currentLine.Peek() != -1) {
-                ISpecies species = ReadAndValidateSpeciesName(currentLine);
-                if (speciesNames.Contains(species.Name))
-                    throw new InputValueException(speciesName.Value.String,
-                                                  "The species {0} appears more than once.", species.Name);
-                speciesNames.Add(species.Name);
-                speciesList.Add(species);
-
-                TextReader.SkipWhitespace(currentLine);
-            }
-            if (speciesNames.Count == 0)
-                throw new InputValueException(); // Missing value
-
-            return new InputValue<List<ISpecies>>(speciesList,
-                                                  string.Join(" ", speciesNames.ToArray()));
         }
 
         //---------------------------------------------------------------------
