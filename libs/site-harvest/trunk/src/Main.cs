97c8678e7aff5308def1abf880dfdad8317db2a5
jimm <jdomingo@green-code.com>
2014-10-23 17:47:44 +0000

    Added logic to the library's initialization so it can handle multiple calls to its initialization method correctly.
    git-svn-id: http://Marc-PC/svn/Full@2567 85f15e0e-5137-b144-a863-98bd542365a3

--- libs/site-harvest/trunk/src/Main.cs
+++ libs/site-harvest/trunk/src/Main.cs
@@ -13,6 +13,8 @@ namespace Landis.Library.SiteHarvest
     /// </summary>
     public static class Main
     {
+        private static bool libInitialized = false;
+
         /// <summary>
         /// Initialize the library for use by client code.
         /// </summary>
@@ -21,9 +23,19 @@ namespace Landis.Library.SiteHarvest
         /// </param>
         public static void InitializeLib(ICore modelCore)
         {
-            Model.Core = modelCore;
-            SiteVars.Initialize();
-            AgeRangeParsing.InitializeClass();
+            // Only initialize the library once.  This method may be called
+            // multiple times if harvest and land-use extensions are both
+            // in a scenario.  The harvest extension initializes the Harvest
+            // Management, which in turns initializes this library.  The Land
+            // Use extension also initializes this library since it's a client
+            // of this library.
+            if (! libInitialized)
+            {
+                Model.Core = modelCore;
+                SiteVars.Initialize();
+                AgeRangeParsing.InitializeClass();
+                libInitialized = true;
+            }
         }
 
         /// <summary>
