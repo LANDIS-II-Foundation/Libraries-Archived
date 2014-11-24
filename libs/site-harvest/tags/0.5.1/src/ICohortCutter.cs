5c26989ed6b150718bb03c2ae8aa9c304150ab5d
jimm <jdomingo@green-code.com>
2014-11-24 05:09:49 +0000

    Tagged version 0.5.1
    git-svn-id: http://Marc-PC/svn/Full@2791 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ libs/site-harvest/tags/0.5.1/src/ICohortCutter.cs
@@ -0,0 +1,33 @@
+﻿// This file is part of the Site Harvest library for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/libs/site-harvest/trunk/
+
+using Landis.SpatialModeling;
+
+namespace Landis.Library.SiteHarvest
+{
+    /// <summary>
+    /// Interface for cutting selected cohorts at a site.
+    /// </summary>
+    public interface ICohortCutter
+    {
+        /// <summary>
+        /// The object responsible for selecting which cohorts to be cut.
+        /// </summary>
+        ICohortSelector CohortSelector { get; }
+
+        /// <summary>
+        /// Cut cohorts at an individual site.
+        /// </summary>
+        /// <param name="site">
+        /// The site where to cut cohorts.
+        /// </param>
+        /// <param name="cohortCounts">
+        /// The number of cohorts cut for each species will be recorded in this
+        /// parameter for the caller's use.
+        /// </param>
+        void Cut(ActiveSite site,
+                 CohortCounts cohortCounts);
+    }
+}
