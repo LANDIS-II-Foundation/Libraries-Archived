de356d4cd13a40a05dc0830292e475d90a28f13e
jimm <jdomingo@green-code.com>
2014-12-01 03:50:37 +0000

    Tagged version 0.2 of the biomass harvest library.
    git-svn-id: http://Marc-PC/svn/Full@2843 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ libs/biomass-harvest/tags/0.2/src/Model.cs
@@ -0,0 +1,18 @@
+﻿// This file is part of the Biomass Harvest library for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/libs/biomass-harvest/trunk/
+
+using Landis.Core;
+
+namespace Landis.Library.BiomassHarvest
+{
+    internal static class Model
+    {
+        /// <summary>
+        /// The model core instance used across the library's internal
+        /// components.
+        /// </summary>
+        internal static ICore Core { get; set; }
+    }
+}
