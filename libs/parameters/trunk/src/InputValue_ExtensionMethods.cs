0d5eef87e7ee46d913a576650a012f02a5d74fdf
amgdebruijn@gmail.com <amgdebruijn@gmail.com>
2014-12-12 15:13:55 +0000

    Reverted to last commit by Jimm, mine was unintended
    git-svn-id: http://Marc-PC/svn/Full@2938 85f15e0e-5137-b144-a863-98bd542365a3

--- libs/parameters/trunk/src/InputValue_ExtensionMethods.cs
+++ libs/parameters/trunk/src/InputValue_ExtensionMethods.cs
@@ -13,25 +13,6 @@ namespace Landis.Library.Parameters
     public static class InputValue_ExtensionMethods
     {
         /// <summary>
-        /// Parses a string to a designated type and throws an error when it is unable to do so
-        /// the advantage over C# designated parse functions is that it avoids the throw catch structures in the code
-        /// </summary>
-        private static T TryParse<T>(string value, string label)
-             where T : System.IComparable<T>
-         {
-            T return_value;
-            try
-            {
-                return_value = (T)System.Convert.ChangeType(value, typeof(T));
-            }
-            catch
-            {
-                throw new System.Exception("Expecting type " + typeof(T) + " for " + label);
-            }
-            return return_value;
-        }
-
-        /// <summary>
         /// Is x &lt; y ?
         /// </summary>
         public static bool LessThan<T>(T x, T y)
@@ -54,11 +35,11 @@ namespace Landis.Library.Parameters
         //--------------------------------------------------------------------
 
         private static string MakeExceptionMessage<T>(string parameterName,
-                                                      T value,
+                                                      InputValue<T> value,
                                                       string error,
                                                       params object[] errorArgs)
         {
-            string message = string.Format("Input value {0}", value);
+            string message = string.Format("Input value {0}", value.String);
             if (parameterName != null)
                 message += " for " + parameterName;
             message += " is " + string.Format(error, errorArgs);
@@ -66,18 +47,7 @@ namespace Landis.Library.Parameters
         }
 
         //--------------------------------------------------------------------
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="inputValue"></param>
-        /// <param name="min"></param>
-        /// <returns></returns>
-        public static T CheckGreaterThanOrEqual<T>(this InputValue<T> inputValue, T min)
-            where T : System.IComparable<T>
-        { 
-            return CheckGreaterThanOrEqual(inputValue,min,inputValue.String);
-        }
+
         /// <summary>
         /// Check if an input value is > or = a minimum value.
         /// </summary>
@@ -91,97 +61,53 @@ namespace Landis.Library.Parameters
         /// <returns>The actual input value if it > or = the minimum.</returns>
         /// <exception cref="InputValueException">
         /// Thrown if the value is &lt; than the minimum.</exception>
-        public static T CheckGreaterThanOrEqual<T>(this T inputValue,
+        public static T CheckGreaterThanOrEqual<T>(this InputValue<T> inputValue,
                                                    T min,
-                                                   string parameterName)
+                                                   string parameterName = null)
             where T : System.IComparable<T>
         {
-            if (LessThan(inputValue, min))
+            if (LessThan(inputValue.Actual, min))
             {
                 string message = MakeExceptionMessage(parameterName, inputValue,
                                                       "< {0}", min);
                 //string message = (parameterName == null)
                 //    ? "Input value {0} is < {1}"
                 //    : "Input value for " + parameterName + " {0} is < {1}";
-                throw new InputValueException(parameterName, message);
+                throw new InputValueException(inputValue.String, message);
             }
-            return inputValue;
+            return inputValue.Actual;
         }
-        /// <summary>
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="inputValue"></param>
-        /// <param name="min"></param>
-        /// <returns></returns>
-        public static T CheckGreaterThanOrEqual<T>(this  InputVar<string> inputValue,
-                                                T min)
-            where T : System.IComparable<T>
-        {
-            T value = TryParse<T>(inputValue.Value.Actual.ToString(), inputValue.Name);
-            return CheckGreaterThanOrEqual<T>(value, min, inputValue.Name);
-        }
-
 
         //--------------------------------------------------------------------
-        /// <summary>
-        /// Check if an input value is &lt; or = a maximum value.
-        /// </summary>
-        /// <example>
-        /// This example checks if a float input value is no more than 0.75.
-        /// <code>
-        /// InputValue&lt;float> fooParam = // read from input
-        /// float foo = fooParam.CheckLessThanOrEqual(0.75, "FooParameter");
-        /// </code>
-        /// </example>
-        /// <returns>The actual input value if it &lt; or = the maximum.</returns>
-        /// <exception cref="InputValueException">
-        /// Thrown if the value is > than the maximum.</exception>
-        public static T CheckLessThanOrEqual<T>(this  InputValue<T> inputValue,
-                                                T max)
-            where T : System.IComparable<T>
-        {
-            return CheckLessThanOrEqual<T>(inputValue.Actual, max, inputValue.String);
-        }
-         
-        /// <summary>
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="inputValue"></param>
-        /// <param name="min"></param>
-        /// <returns></returns>
-        public static T CheckGreaterThanOrEqual<T>(this  InputValue<string> inputValue,
-                                                T min)
-            where T : System.IComparable<T>
-        {
-            T value = TryParse<T>(inputValue, inputValue.String);
-            return CheckGreaterThanOrEqual<T>(value, min, inputValue.String);
-        }
-        /// <summary>
-        /// Check if an input value is &lt; or = a maximum value.
-        /// </summary>
-        /// <example>
-        /// This example checks if a float input value is no more than 0.75.
-        /// <code>
-        /// InputValue&lt;float> fooParam = // read from input
-        /// float foo = fooParam.CheckLessThanOrEqual(0.75, "FooParameter");
-        /// </code>
-        /// </example>
-        /// <returns>The actual input value if it &lt; or = the maximum.</returns>
-        /// <exception cref="InputValueException">
-        /// Thrown if the value is > than the maximum.</exception>
-        public static T CheckLessThanOrEqual<T>(this  T inputValue,
-                                                T max,
-                                                string parameterName)
-            where T : System.IComparable<T>
-        {
-            if (GreaterThan(inputValue, max))
-            {
-                string message = MakeExceptionMessage(parameterName, inputValue, "> {0}", max);
 
-                throw new InputValueException(parameterName, message);
+        /// <summary>
+        /// Check if an input value is &lt; or = a maximum value.
+        /// </summary>
+        /// <example>
+        /// This example checks if a float input value is no more than 0.75.
+        /// <code>
+        /// InputValue&lt;float> fooParam = // read from input
+        /// float foo = fooParam.CheckLessThanOrEqual(0.75, "FooParameter");
+        /// </code>
+        /// </example>
+        /// <returns>The actual input value if it &lt; or = the maximum.</returns>
+        /// <exception cref="InputValueException">
+        /// Thrown if the value is > than the maximum.</exception>
+        public static T CheckLessThanOrEqual<T>(this InputValue<T> inputValue,
+                                                T max,
+                                                string parameterName = null)
+            where T : System.IComparable<T>
+        {
+            if (GreaterThan(inputValue.Actual, max))
+            {
+                string message = MakeExceptionMessage(parameterName, inputValue,
+                                                      "> {0}", max);
+                throw new InputValueException(inputValue.String, message);
             }
-            return inputValue;
+            return inputValue.Actual;
         }
+
+        //--------------------------------------------------------------------
 
         /// <summary>
         /// Check if an input value is within a specified range.
@@ -196,43 +122,20 @@ namespace Landis.Library.Parameters
         /// <returns>The actual input value if it is within range.</returns>
         /// <exception cref="InputValueException">
         /// Thrown if the value is outside the range.</exception>
-        public static T CheckInRange<T>(T inputValue,
+        public static T CheckInRange<T>(this InputValue<T> inputValue,
                                         T min,
                                         T max,
-                                        string parameterName)
+                                        string parameterName = null)
             where T : System.IComparable<T>
         {
-            if (LessThan(inputValue, min) || GreaterThan(inputValue, max))
+            if (LessThan(inputValue.Actual, min) || GreaterThan(inputValue.Actual, max))
             {
-                string message = MakeExceptionMessage(parameterName, inputValue, "not between {0:0.0} and {1:0.0}", min, max);
-
-
-                throw new InputValueException(parameterName, message);
+                string message = MakeExceptionMessage(parameterName, inputValue,
+                                                      "not between {0:0.0} and {1:0.0}",
+                                                      min, max);
+                throw new InputValueException(inputValue.String, message);
             }
-            return inputValue;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="inputValue"></param>
-        /// <param name="min"></param>
-        /// <param name="max"></param>
-        /// <returns></returns>
-        public static T CheckInRange<T>(this  InputVar<string> inputValue,
-                                        T min,
-                                        T max)
-            where T : System.IComparable<T>
-        {
-            T value = TryParse<T>(inputValue.Value.Actual.ToString(), inputValue.Name);
-            if (LessThan(value, min) || GreaterThan(value, max))
-            {
-                string message = MakeExceptionMessage(inputValue.Name, inputValue.Value, "not between {0:0.0} and {1:0.0}", min, max);
-
-
-                throw new InputValueException(inputValue.Name, message);
-            }
-            return value;
+            return inputValue.Actual;
         }
     }
 }
