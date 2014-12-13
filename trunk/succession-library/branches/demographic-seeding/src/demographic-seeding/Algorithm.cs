e3535207b8fb3cf0dbc7ecf6db5133e3383af61d
jimm <jdomingo@green-code.com>
2014-12-13 03:38:56 +0000

    Corrected the value that's passed as the "max_age_steps" parameter to the Map's constructor.
    git-svn-id: http://Marc-PC/svn/Full@2947 85f15e0e-5137-b144-a863-98bd542365a3

--- trunk/succession-library/branches/demographic-seeding/src/demographic-seeding/Algorithm.cs
+++ trunk/succession-library/branches/demographic-seeding/src/demographic-seeding/Algorithm.cs
@@ -25,6 +25,14 @@ namespace Landis.Library.Succession.DemographicSeeding
                 if (species.Longevity > maxCohortAge)
                     maxCohortAge = species.Longevity;
 
+            // The library's code comments say max_age_steps represents
+            // "maximum age allowed for any species, in years", but it's
+            // used in the code as though it represents the maximum age of
+            // any species' cohort IN NUMBER OF SUCCESSION TIMESTEPS.  So if
+            // the oldest species' Longevity is 2,000 years, and the succession
+            // timestep is 10 years, then max_age_steps is 200 timesteps.
+            int max_age_steps = maxCohortAge / successionTimestep;
+
             seedingData = new Seed_Dispersal.Map(
                 Model.Core.Landscape.Columns,
                 Model.Core.Landscape.Rows,
@@ -32,7 +40,7 @@ namespace Landis.Library.Succession.DemographicSeeding
                 numTimeSteps,
                 successionTimestep,
                 Model.Core.Ecoregions.Count,
-                maxCohortAge);
+                max_age_steps);
             seedingData.pixel_size = Model.Core.CellLength;
 
             // Initialize some species parameters from the core.
