c4a2465f7706018a7c833d47c0f6d21e52cf9ff0
lbross@pdx.edu <lbross@pdx.edu>
2015-01-22 18:52:39 +0000

    Updates to release biomass-harvest library v1.0.0 rc1
    git-svn-id: http://Marc-PC/svn/Full@3151 85f15e0e-5137-b144-a863-98bd542365a3

--- libs/biomass-harvest/trunk/src/AssemblyInfo.cs
+++ libs/biomass-harvest/trunk/src/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Biomass Harvest Library")]
-[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyDescription("Harvest Library for LANDIS-II")]
-[assembly: AssemblyCopyright("2014 University of Notre Dame")]
+[assembly: AssemblyCopyright("2015 University of Notre Dame")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
