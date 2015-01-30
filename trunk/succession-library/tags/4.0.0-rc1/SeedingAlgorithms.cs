ea133c8a70037afc7458407de5b9d86aac5ddedc
Brian <brmirand@gmail.com>
2015-01-30 16:50:15 +0000

    Tag for 4.0.0-rc1
    git-svn-id: http://Marc-PC/svn/Full@3200 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/succession-library/tags/4.0.0-rc1/SeedingAlgorithms.cs
@@ -0,0 +1,12 @@
+namespace Landis.Library.Succession
+{
+    /// <summary>
+    /// Seeding algorithms
+    /// </summary>
+    public enum SeedingAlgorithms
+    {
+        NoDispersal = 0,
+        UniversalDispersal,
+        WardSeedDispersal
+    }
+}
