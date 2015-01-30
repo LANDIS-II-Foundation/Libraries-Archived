117f7f950736c2a0d34d7f4da8ecc600a292fd5b
Brian <brmirand@gmail.com>
2015-01-30 16:42:56 +0000

    Add browse branch
    git-svn-id: http://Marc-PC/svn/Full@3198 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/biomass-cohort-library/branches/browse/src/CohortGrowthReduction.cs
@@ -0,0 +1,70 @@
+//  Copyright 2005-2010 Portland State University, University of Wisconsin
+//  Authors:  Robert M. Scheller, James B. Domingo
+
+using Landis.Core;
+using Landis.SpatialModeling;
+using Edu.Wisc.Forest.Flel.Util;
+
+
+namespace Landis.Library.BiomassCohorts
+{
+    public class CohortGrowthReduction
+    {
+        /// <summary>
+        /// Various delegates associated with growth reduction.
+        /// </summary>
+        public static class Delegates
+        {
+            /// <summary>
+            /// A method to compute how much cohort growth is reduced at a
+            /// site.
+            /// </summary>
+            public delegate double Compute(ICohort    cohort,
+                                           ActiveSite site);
+                                           //int        siteBiomass);
+        }
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// Defaults for defoliation.
+        /// </summary>
+        public static class Defaults
+        {
+            /// <summary>
+            /// Default method for computing how much a cohort is defoliated at
+            /// a site.
+            /// </summary>
+            /// <returns>
+            /// 0%
+            /// </returns>
+            public static double Compute(ICohort    cohort,
+                                         ActiveSite site)
+                                         //int        siteBiomass)
+            {
+                return 0.0;
+            }
+        }
+
+        //---------------------------------------------------------------------
+
+        private static Delegates.Compute computeMethod = Defaults.Compute;
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// The method to compute how much a cohort is defoliated at a site.
+        /// </summary>
+        public static Delegates.Compute Compute
+        {
+            get {
+                return computeMethod;
+            }
+
+            set {
+                Require.ArgumentNotNull(value);
+                computeMethod = value;
+            }
+        }
+    }
+}
