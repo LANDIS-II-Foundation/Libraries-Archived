085239307bd66e0ecb54b484392929dc4bd2305b
jimm <jdomingo@green-code.com>
2015-02-19 00:06:09 +0000

    Tagged the official release of version 1.0.0
    git-svn-id: http://Marc-PC/svn/Full@3325 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ libs/biomass-harvest/tags/1.0.0/src/CohortCutterFactory.cs
@@ -0,0 +1,39 @@
+﻿// This file is part of the Biomass Harvest library for LANDIS-II.
+// For copyright and licensing information, see the NOTICE and LICENSE
+// files in this project's top-level directory, and at:
+//   http://landis-extensions.googlecode.com/svn/libs/biomass-harvest/trunk/
+
+using Landis.Core;
+using Landis.Library.SiteHarvest;
+
+namespace Landis.Library.BiomassHarvest
+{
+    /// <summary>
+    /// A factory for making instances of cohort cutters (WholeCohortCutter
+    /// and PartialCohortCutter).
+    /// </summary>
+    public static class CohortCutterFactory
+    {
+        /// <summary>
+        /// Creates a cohort cutter instance.
+        /// </summary>
+        /// <returns>
+        /// An instance of WholeCohortCutter if no species is partially thinned
+        /// by the cohort selector.  If the selector has a percentage for at
+        /// least one species, then an instance of PartialCohortCutter is
+        /// returned.
+        /// </returns>
+        public static ICohortCutter CreateCutter(ICohortSelector cohortSelector,
+                                                 ExtensionType   extensionType)
+        {
+            ICohortCutter cohortCutter;
+            if (PartialThinning.CohortSelectors.Count == 0)
+                cohortCutter = new WholeCohortCutter(cohortSelector, extensionType);
+            else
+                cohortCutter = new PartialCohortCutter(cohortSelector,
+                                                       PartialThinning.CohortSelectors,
+                                                       extensionType);
+            return cohortCutter;
+        }
+    }
+}
