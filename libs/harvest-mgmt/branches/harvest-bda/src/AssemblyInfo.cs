f88879fd3b87f7755de0342950aa7aaebfbc2779
Brian <brmirand@gmail.com>
2015-04-21 21:32:27 +0000

    Merge changes from trunk.
    git-svn-id: http://Marc-PC/svn/Full@3639 85f15e0e-5137-b144-a863-98bd542365a3

--- libs/harvest-mgmt/branches/harvest-bda/src/AssemblyInfo.cs
+++ libs/harvest-mgmt/branches/harvest-bda/src/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Harvest Library")]
-[assembly: AssemblyVersion("0.7.*")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyDescription("Harvest Library for LANDIS-II")]
-[assembly: AssemblyCopyright("2014 University of Notre Dame")]
+[assembly: AssemblyCopyright("2015 University of Notre Dame")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
