1f8d5b7fc7744a34efcad20a513b4e3fe95aca7b
Rob <rmscheller@gmail.com>
2015-05-25 18:41:21 +0000

    Removing extraneous code to ease migration to GitHub.
    git-svn-id: http://Marc-PC/svn/Full@3716 85f15e0e-5137-b144-a863-98bd542365a3

--- trunk/plug-in-admin-library/tags/iteration-13/IsNameInUseMethod.cs
+++
@@ -1,8 +0,0 @@
-namespace Landis.PlugIns.Admin
-{
-	/// <summary>
-	/// A method for checking if an extension name is already in use in the
-	/// plug-in dataset.
-	/// </summary>
-	public delegate bool IsNameInUseMethod(string name);
-}
